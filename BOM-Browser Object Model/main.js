5a9c34e169963b9b48d62dc6d835610209fba1d2
StefanKostic88 <stefankostic88@yahoo.com>
2022-12-01 19:51:12 +0100

    BOM intro

---
+++ BOM-Browser Object Model/main.js
@@ -0,0 +1,16 @@
+var a = 50;
+
+// console.log(a + b);
+
+// DOM
+// console.log(window.document); // ovde se nalazi DOM
+
+// BOM kolekcija objekata koji su ugradjeni i vezani su za browser
+// navigator screen location itd
+
+// svaka globalna varijabla ili funkcija se smesta kao properti window objecta
+
+console.log(window.a);
+console.log(window.history);
+
+// recoil beiblioteka za cuvanje podataka
